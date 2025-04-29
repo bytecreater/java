0f515e431aa0b8acfa10b405155280d5cab7c4df
bytecreater <nihalahemad2003@gmail.com>
2025-04-29 17:05:46 +0530

    Insertion Sort

---
+++ DSA/Sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {7,8,3,4,1};
+        //Insertion Sort
+        //for unsoreted
+        for (int i = 1; i < arr.length; i++){
+            int current = arr[i];
+            //for sorted
+            int j = i-1;
+            while(j >= 0 && current < arr[j]){
+                
+            }
+        }
+    }
+    
+}
