170c672d0ff8b9c78806c37b7b1d0bf6f8d3b536
bytecreater <nihalahemad2003@gmail.com>
2025-04-29 17:07:58 +0530

    update

--- DSA/Sorting/InsertionSort.java
+++ DSA/Sorting/InsertionSort.java
@@ -1,5 +1,10 @@
 import java.util.*;
 public class InsertionSort {
+    public static void Display(int arr[]){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {7,8,3,4,1};
         //Insertion Sort
@@ -9,9 +14,13 @@ public class InsertionSort {
             //for sorted
             int j = i-1;
             while(j >= 0 && current < arr[j]){
-                
+                arr[j+1]=arr[j];
+                j--;
             }
+            //placement
+            arr[j+1] = current;
         }
+        Display(arr);
     }
     
 }
